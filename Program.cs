f9b8849f0b9c66a9e15d6bb646ea3679997094ca
«Aliya <alyakalimova@mail.ru>
2022-05-28 19:57:58 +0600

    first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter your number: ");
+int a = int.Parse(Console.ReadLine());
+int result = a * a;
+Console.Write("Square of your number: ");
+Console.WriteLine(result);
