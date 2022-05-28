d64f33f55911b581b0332bd3d9a3a80965fda8e6
«Aliya <alyakalimova@mail.ru>
2022-05-28 21:52:58 +0600

    square number

--- task 2/Program.cs
+++ task 2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// проверять квадрат по двум заданным числам
+Console.Write("Введите число numbera ");
+int numbera = int.Parse(Console.ReadLine());
+Console.Write("Введите число numberb ");
+int numberb = int.Parse(Console.ReadLine());
+if (numbera == numberb*numberb)
+{
+    Console.Write("Да, первое число является квадратом второго");
+}
+else
+{
+    Console.Write("Нет, первое число не является квадратом второго");
+}
